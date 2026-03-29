3b3e5fa6119d9ae93c307c2be38ba7b26243dc10
mikedavissoftware <mikedavissoftware@gmail.com>
2026-03-28 23:33:26 -0600

    login and signup form functional, pre-API connection

--- front-end/src/pages/Signup.jsx
+++ front-end/src/pages/Signup.jsx
@@ -3,66 +3,75 @@ import { useState } from "react"
 
 function Signup() {
   const newForm = {
-      name: "",
-      role: "",
-      isAdmin: false,
-      username: "",
-      email: "",
-      password: ""
+    name: "",
+    role: "",
+    isAdmin: false,
+    username: "",
+    email: "",
+    password: ""
+  }
+
+  const [formData, setFormData] = useState(newForm)
+
+  function handleChange(e) {
+    const key = e.target.name
+    let value = e.target.value
+    if (key === "isAdmin") {
+      if (value === "true") {
+        value = true
+      } else (
+        value = false
+      )
     }
-  
-    const [formData, setFormData] = useState(newForm)
-  
-    function handleChange(e) {
-      setFormData({...formData, [e.target.name]: e.target.value})
-      console.log(formData)
-    }
-  
-    function showFormData(e) {
-      e.preventDefault
-      console.log(formData)
-    }
-  
-    function handleSubmit(e) {
-      e.preventDefault
-      console.log(formData)
-    }
-  
-    return (
-      <div id="login-form" className="p-10">
-        <h2 className="text-2xl font-bold">Sign Up</h2>
-  
-        <form onSubmit={handleSubmit} >
+    setFormData({...formData, [key]: value})
+    console.log(formData)
+  }
 
-          <label for="name">Name:</label><br/>
-          <input type="text" placeholder="Please Enter Name..." id="name" name="name" value={formData.name} onChange={handleChange} /><br/>
+  function showFormData(e) {
+    e.preventDefault
+    console.log(formData)
+  }
 
-          <label for="role">Role:</label><br/>
-          <input type="text" placeholder="Please Enter Role..." id="role" name="role" value={formData.role} onChange={handleChange} /><br/>
+  function handleSubmit(e) {
+    e.preventDefault
+    console.log(formData)
+  }
 
-          <label for="is-admin">Is Admin:</label><br/>
-          <select type="checkbox" id="is-admin" name="isAdmin" value={formData.isAdmin} onChange={handleChange} >
-            <option value={true}>Yes</option>
-            <option value={false}>No</option>
-          </select><br/>
+  return (
+    <div id="signup-form" className="p-10">
+      <h2 className="text-2xl font-bold">Sign Up</h2>
 
-          <label for="email">Email:</label><br/>
-          <input type="email" placeholder="Please Enter Email..." id="email" name="email" value={formData.email} onChange={handleChange} /><br/>
+      <form onSubmit={handleSubmit} >
 
-          <label for="username">Username:</label><br/>
-          <input type="text" placeholder="Please Enter Username..." id="username" name="username" value={formData.username} onChange={handleChange} /><br/>
+        <label htmlFor="name">Name:</label><br/>
+        <input type="text" placeholder="Please Enter Name..." id="name" name="name" value={formData.name} onChange={handleChange} /><br/>
 
-          <label for="password">Password:</label><br/>
-          <input type="password" placeholder="Please Enter Password..." id="password" name="password" value={formData.password} onChange={handleChange} /><br/>
+        <label htmlFor="role">Role:</label><br/>
+        <input type="text" placeholder="Please Enter Role..." id="role" name="role" value={formData.role} onChange={handleChange} /><br/>
 
-  
-          <button type="submit" className="bg-green-300 rounded-md px-2">Submit</button>
-        </form>
-  
-        <br/>
-        <button onClick={showFormData} className="bg-slate-300 rounded-md px-2">console.log(formData)</button>
-      </div>
-    );
+        <label htmlFor="is-admin">Is Admin:</label><br/>
+        <select type="checkbox" id="is-admin" name="isAdmin" value={formData.isAdmin} onChange={handleChange} >
+          <option value="true">Yes</option>
+          <option value="false">No</option>
+        </select><br/>
+
+        <label htmlFor="email">Email:</label><br/>
+        <input type="email" placeholder="Please Enter Email..." id="email" name="email" value={formData.email} onChange={handleChange} /><br/>
+
+        <label htmlFor="username">Username:</label><br/>
+        <input type="text" placeholder="Please Enter Username..." id="username" name="username" value={formData.username} onChange={handleChange} /><br/>
+
+        <label htmlFor="password">Password:</label><br/>
+        <input type="password" placeholder="Please Enter Password..." id="password" name="password" value={formData.password} onChange={handleChange} /><br/>
+
+
+        <button type="submit" className="bg-green-300 rounded-md px-2">Submit</button>
+      </form>
+
+      <br/>
+      <button onClick={showFormData} className="bg-slate-300 rounded-md px-2">console.log(formData)</button>
+    </div>
+  );
 }
 
 export default Signup;
